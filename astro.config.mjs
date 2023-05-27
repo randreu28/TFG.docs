5db7cbf66a542d26aa63e60aedbd060bd7c9b7a9
Rubén Chiquin <randreu28@gmail.com>
2023-05-27 22:11:53 +0200

    Adds a dark cyan theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import customTheme from './ocean-tokyo-dark.json';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      // @ts-ignore
+      theme: customTheme,
+    }
+  }
 });
