a0a1d0ae64e689ecf0534bd4a00d603cf34e3bc5
Rubén Chiquin <randreu28@gmail.com>
2023-05-28 17:49:44 +0200

    Adds preact for creating interactivity islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import customTheme from './ocean-tokyo-dark.json';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   markdown: {
     shikiConfig: {
       // @ts-ignore
-      theme: customTheme,
+      theme: customTheme
     }
   }
 });
