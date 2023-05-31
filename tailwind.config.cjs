cc5c20397daa3e447f74a7ec539a6e9698a62fa2
Rubén Chiquin <randreu28@gmail.com>
2023-05-31 12:05:24 +0200

    Updates the font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Lora Variable", "sans-serif", ...defaultTheme.fontFamily.sans],
+			},
 			typography: {
 				quoteless: {
 					css: {
